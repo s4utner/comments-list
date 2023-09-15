f3747e5c76b291c5666fbc38b8ef2f25b2722762
Denis <den.sautner@mail.ru>
2023-09-15 15:17:07 +0600

    added files 'main.js', 'api.js' and 'date.js'

---
+++ api.js
@@ -0,0 +1,28 @@
+export const getComments = () => {
+    return fetch('https://wedev-api.sky.pro/api/v1/sautner-denis/comments', {
+        method: 'GET',
+    })
+        .then((response) => {
+            return response.json();
+        })
+};
+
+export const pushComment = ({ safeNameInputValue, safeCommentInputValue }) => {
+    return fetch('https://wedev-api.sky.pro/api/v1/sautner-denis/comments', {
+        method: 'POST',
+        body: JSON.stringify({
+            text: safeCommentInputValue,
+            name: safeNameInputValue,
+            forceError: false,
+        }),
+    }).then((response) => {
+        if (response.status === 400) {
+            throw new Error('Имя и комментарий должны быть не короче 3 символов');
+        } else if (response.status === 500) {
+            pushApiComment();
+            throw new Error('Сервер прилег поспать. Подтвердите повторную попытку отправки');
+        } else {
+            return response.json();
+        }
+    })
+}
