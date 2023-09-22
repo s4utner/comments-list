ea5d3a81c8c85d389e5391a4bb286a363917bc71
Denis <den.sautner@mail.ru>
2023-09-22 18:51:39 +0600

    almost finished hw

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 import { getComments, pushComment } from "./api.js";
 import { date } from "./date.js";
+import { renderComments } from "./renderComments.js";
+import { inputsOnPush } from "./inputsOnPush.js";
+import { initEventListeners } from "./like.js";
 
 // Переменные
 const addButtonElement = document.querySelector(".add-form-button");
@@ -31,7 +34,7 @@ const getApiComments = () => {
                 };
             })
             comment = appComments;
-            renderComment();
+            renderComments({ comment, comments, initEventListeners, quoteGlobal, commentInput, nameInput, addButtonElement });
         })
         .then(() => {
             pageLoader.textContent = "";
@@ -49,123 +52,8 @@ const getApiComments = () => {
 
 let comment = [];
 
-// Поставить лайк
-const initEventListeners = () => {
-    const likeButtonElement = document.querySelectorAll('.like-button');
-    for (const item of likeButtonElement) {
-        item.addEventListener('click', (event) => {
-            event.stopPropagation();
-            const index = item.dataset.index;
-            if (comment[index].isLiked) {
-                comment[index].isLiked = false;
-                comment[index].likes--;
-            } else {
-                comment[index].isLiked = true;
-                comment[index].likes++;
-            }
-            renderComment();
-        });
-    };
-};
-
-// Рендер комментариев
-const renderComment = () => {
-    const commentsHTML = comment
-        .map((comment, index) => {
-            return `<li class="comment" data-index="${index}">
-        <div class="comment-header">
-          <div>${comment.name}</div>
-          <div class="date">${comment.date}</div>
-        </div>
-        ${comment.quote
-                    ? `<div
-                      class="quote-form-text"
-                      rows="2"
-                      >${comment.quote}</div>`
-                    : ``
-                }
-        ${comment.isEdit
-                    ? `<textarea
-                      class="add-form-text"
-                      rows="2"
-                    >${comment.text}</textarea>`
-                    : `<div class="comment-body"><div class="comment-text" data-index="${index}">${comment.text}</div></div>`
-                }
-        <div class="comment-footer">
-                        ${comment.isEdit
-                    ? `<button data-index="${index}" class='save-form-button'>Сохранить изменения</button>`
-                    : `<button data-index="${index}" class='edit-form-button'>Редактировать комментарий</button>`
-                }
-          <div class="likes">
-            <span class="likes-counter">${comment.likes}</span>
-            <button class="like-button ${comment.isLiked ? '-active-like' : ''}" data-index="${index}"></button>
-          </div>
-        </div>
-      </li>`;
-        })
-        .join('');
-    comments.innerHTML = commentsHTML;
-
-    //Редактировать комментарий
-    const editButtonElements = document.querySelectorAll('.edit-form-button');
-
-    for (const editButton of editButtonElements) {
-
-        editButton.addEventListener('click', (event) => {
-            event.stopPropagation();
-            const index = editButton.dataset.index;
-            const editComment = comment[index];
-            editComment.isEdit = true;
-
-            renderComment();
-
-            document.querySelector(".add-form-text").focus();
-        });
-    };
-    const saveButtons = document.querySelectorAll(".save-form-button");
-    for (const saveButton of saveButtons) {
-        saveButton.addEventListener("click", (e) => {
-            e.stopPropagation();
-            const saveComment = comment[saveButton.dataset.index];
-            const editFormText = saveButton
-                .closest(".comment")
-                .querySelector(".add-form-text");
-            saveComment.text = editFormText.value;
-
-            saveComment.isEdit = false;
-            renderComment();
-        });
-    }
-
-    // Блокировка кнопки
-    document.addEventListener("input", () => {
-        nameInput.value != "" && commentInput.value != ""
-            ? (addButtonElement.classList.remove('disabled'),
-                addButtonElement.disabled = false,
-                nameInput.classList.remove('error'),
-                commentInput.classList.remove('error'))
-            : (addButtonElement.classList.add('disabled'),
-                addButtonElement.disabled = true);
-    });
-
-    // Ответ на комменатрий
-    const pushedComments = document.querySelectorAll('.comment-text');
-
-    for (const repliedComment of pushedComments) {
-        repliedComment.addEventListener('click', () => {
-            const index = repliedComment.dataset.index;
-
-            const commentQuote = comment[index];
-            quoteGlobal = `> ${commentQuote.name}:\n${commentQuote.text}`;
-            commentInput.value = `"${quoteGlobal}"\n`;
-
-        });
-    };
-
-    initEventListeners();
-};
 getApiComments();
-renderComment();
+renderComments({ comment, comments, initEventListeners, quoteGlobal, commentInput, nameInput, addButtonElement });
 
 //Добавление комментариев в API
 const pushApiComment = () => {
@@ -197,8 +85,8 @@ const pushApiComment = () => {
             });
             commentLoader.classList.add('delete');
             addForm.classList.remove('delete');
-            initEventListeners();
-            renderComment();
+            initEventListeners({ comment, renderComments, comments, comment, quoteGlobal, commentInput, nameInput, addButtonElement });
+            renderComments({ comment, comments, initEventListeners, quoteGlobal, commentInput, nameInput, addButtonElement });
         })
         .then(() => {
             quoteGlobal = '';
@@ -217,52 +105,22 @@ const pushApiComment = () => {
         });
 }
 
-//Добавление комментария 
-const inputsOnPush = () => {
-    addButtonElement.disabled = false;
-    addButtonElement.classList.remove('disabled');
-    nameInput.classList.remove('error');
-    commentInput.classList.remove('error');
-
-    if (nameInput.value === '' && commentInput.value === '') {
-        nameInput.classList.add('error');
-        commentInput.classList.add('error');
-        addButtonElement.classList.add('disabled');
-        addButtonElement.disabled = true;
-        return;
-    };
-
-    if (nameInput.value === '') {
-        nameInput.classList.add('error');
-        addButtonElement.classList.add('disabled');
-        addButtonElement.disabled = true;
-        return;
-    };
-
-    if (commentInput.value === '') {
-        commentInput.classList.add('error');
-        addButtonElement.classList.add('disabled');
-        addButtonElement.disabled = true;
-        return;
-    };
-
-    pushApiComment();
-};
-
 // Обработчик на кнопке 'Написать'
-addButtonElement.addEventListener('click', inputsOnPush);
+addButtonElement.addEventListener('click', function () {
+    inputsOnPush({ addButtonElement, nameInput, commentInput, pushApiComment });
+});
 
 // Срабатывание кнопки 'Написать' при клике на Enter
 document.addEventListener('keyup', function (enter) {
     if (enter.keyCode === 13) {
-        inputsOnPush();
+        inputsOnPush({ addButtonElement, nameInput, commentInput, pushApiComment });
     }
 });
 
 // Удаление последнего комментария
 deleteButtonElement.addEventListener('click', () => {
     const askForDeleteComment = confirm('Вы уверены, что хотите удалить последний комментарий?') ? comment.pop() : '';
-    renderComment();
+    renderComments({ comment, comments, initEventListeners, quoteGlobal, commentInput, nameInput, addButtonElement });
 });
 
 console.log("It works!");
